e30142530594848f5c5938a6866fea3723bc1c10
omrilotan <omri@hamadgera.com>
2020-08-09 10:06:46 +0300

    lets publish

---
+++ README.md
@@ -0,0 +1,17 @@
+# publish [![](https://img.shields.io/npm/v/@lets/publish.svg)](https://www.npmjs.com/package/@lets/publish) [![](https://img.shields.io/badge/source--000000.svg?logo=github&style=social)](https://github.com/omrilotan/publish)
+
+## 🛵 Publish only if this version was not already published
+
+```bash
+npx @lets/publish@0
+```
+
+Options:
+```bash
+npx @lets/publish [--help] [--dry-run]
+```
+
+| Option | Meaning
+| - | -
+| --help | Print this help document
+| --dry-run | Declare what you were going to do but don't do it
---
+++ bin.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const { resolve } = require('path');
+const execute = require('async-execute');
+const manual = require('./manual');
+
+const [, , ...args] = process.argv;
+
+const options = args.reduce(
+	(accumulator, arg) => {
+		if (arg.startsWith('--')) {
+			accumulator[arg.replace(/^--/, '').toLowerCase()] = true;
+		}
+		return accumulator;
+	},
+	{}
+);
+
+start();
+
+async function start() {
+	if (options.help) {
+		await manual();
+		return;
+	}
+
+	const { name, version } = require(resolve('./package.json'));
+	const exec = options['dry-run']
+		? command => console.log(`$ ${command}`)
+		: command => execute(command, { pipe: true })
+	;
+
+	process.stdout.write(`Check if ${name}@${version} was already published...`);
+	const exists = await execute(`npm view ${name}@${version} version`);
+
+	if (exists) {
+		process.stdout.write(' ...yup\n');
+		return;
+	}
+
+	process.stdout.write('\n');
+
+	await exec('npm publish');
+};
